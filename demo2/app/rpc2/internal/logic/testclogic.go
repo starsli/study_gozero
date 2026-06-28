75aacb00f9502904b1d8bb5b66f1349a5c455cda
starsli <starsli@tencent.com>
2026-06-28 15:37:16 +0800

    save

---
+++ demo2/app/rpc2/internal/logic/testclogic.go
@@ -0,0 +1,29 @@
+package logic
+
+import (
+	"context"
+
+	"demo2/app/rpc2/internal/svc"
+	"demo2/app/rpc2/rpc2_pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type TestCLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewTestCLogic(ctx context.Context, svcCtx *svc.ServiceContext) *TestCLogic {
+	return &TestCLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *TestCLogic) TestC(in *rpc2_pb.TestCReq) (*rpc2_pb.TestCRsp, error) {
+	// todo: add your logic here and delete this line
+	return &rpc2_pb.TestCRsp{OutputParams: in.InputParams + "starsli2"}, nil
+}
---
+++ demo2/app/rpc2/rpc2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"demo2/app/rpc2/internal/config"
+	"demo2/app/rpc2/internal/server"
+	"demo2/app/rpc2/internal/svc"
+	"demo2/app/rpc2/rpc2_pb"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/rpc2.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		rpc2_pb.RegisterRpc2Server(grpcServer, server.NewRpc2Server(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
